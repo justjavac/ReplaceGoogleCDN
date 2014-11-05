99f2b44d74e23e31b68095504b7a9a532b9fbf72
dishuostec <dishuostec@gmail.com>
2014-11-05 16:47:18 +0800

    url被覆盖

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
         var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
-        url = request.url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
         return {redirectUrl: url};
     },
     {
