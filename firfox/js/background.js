cbb5cbd91462ddf7e3644f990be1588d1b81bf36
saowang <saowang@outlook.com>
2017-06-04 16:08:04 +0800

    更新结构,方便以后拓展

--- firfox/js/background.js
+++ firfox/js/background.js
@@ -1,14 +1,30 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(request) {
-        var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
-        url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
-        return {redirectUrl: url};
+var block = {
+    ajax: {
+        slow: "ajax.googleapis.com",
+        good: "ajax.proxy.ustclug.org"
     },
-    {
-        urls: [
-            "*://ajax.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
-        ]
-    },
-    ["blocking"]
-);
+    themes: {
+        slow: "themes.googleusercontent.com",
+        good: "google-themes.proxy.ustclug.org"
+    }
+};
+
+var filter = function() {
+    var result = [];
+    for (var i in block) {
+        result.push("*://" + block[i].slow + "/*");
+    }
+    return {
+        urls: result
+    }
+};
+chrome.webRequest.onBeforeRequest.addListener(function(request) {
+    for (var i in block) {
+        if (request.url.match(block[i].slow)) {
+            //console.log("slow: " + block[i].slow + "\ngood: " + block[i].good);
+            return {
+                redirectUrl: request.url.replace(block[i].slow, block[i].good)
+            }
+        }
+    }
+}, filter(), ["blocking"]);
