8c4fa9b0ee579f96f1de9def3704029044998138
YuxuanZuo <yuxuan.zuo@outlook.com>
2020-03-23 13:47:17 +0800

    强制使用https (#48)

--- firfox/js/background.js
+++ firfox/js/background.js
@@ -1,7 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
-        var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
+        var url = request.url.replace('http://', 'https://')
+        url = url.replace('googleapis.com', 'proxy.ustclug.org');
         url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        url = url.replace('www.google.com/recaptcha/','www.recaptcha.net/recaptcha/');
         return {redirectUrl: url};
     },
     {
