1631cc1f4b39f63535e21ff6991c4ae727a595c4
Frost Wong <happyhackerwqc@foxmail.com>
2021-01-11 16:11:32 +0800

    Fix bootstrapcdn issue (#66)

--- firfox/js/background.js
+++ firfox/js/background.js
@@ -4,12 +4,14 @@ chrome.webRequest.onBeforeRequest.addListener(
         url = url.replace('googleapis.com', 'proxy.ustclug.org');
         url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
         url = url.replace('www.google.com/recaptcha/','www.recaptcha.net/recaptcha/');
+        url = url.replace('maxcdn.bootstrapcdn.com/bootstrap/','cdn.bootcdn.net/ajax/libs/twitter-bootstrap/');
         return {redirectUrl: url};
     },
     {
         urls: [
             "*://ajax.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
+            "*://themes.googleusercontent.com/*",
+            "*://maxcdn.bootstrapcdn.com/bootstrap/*"
         ]
     },
     ["blocking"]
