a454a4fe28bc1668b5657cc2f9d02c38f975b7b1
好吧，你想说啥 <zonghengbaihe521@qq.com>
2022-03-03 08:57:59 +0800

    remove CSP  (#81)
    * add func remove CSP
    
    * 新增参考文档
    
    * 添加测试用例

--- extension/js/background.js
+++ extension/js/background.js
@@ -10,20 +10,74 @@ const tabinfo = new Map();
  * @returns {boolean}
  */
 function hasCSP(headers = []) {
-  return headers.some(
-    (x) => x.name.toLowerCase() === "content-security-policy"
-  );
+    return headers.some(
+        (x) => x.name.toLowerCase() === "content-security-policy"
+    );
 }
 
+
+/**
+ * 响应头里CSP相关的选项
+ * @type {string[]}
+ */
+const removeCSP=[
+    'content-security-policy',
+    'content-security-policy-report-only',
+    'expect-ct',
+    'report-to',
+    'x-content-security-policy',
+    'x-webkit-csp',
+    'x-xss-protection',
+    'x-permitted-cross-domain-policies',
+    'x-content-type-options',
+    'x-frame-options',
+    'permissions-policy',
+    'timing-allow-origin'
+];
+
+/**
+ * 移除CSP
+ * 参考文档：
+ *   1、 https://developer.chrome.com/docs/extensions/reference/webRequest/#event-onHeadersReceived
+ *   2、  https://chromium.googlesource.com/chromium/src/+/refs/heads/main/docs/trusted_types_on_webui.md
+ *   3、 https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/trusted-types
+ *   4、 Arrow_Function 箭头函数
+ *   5、 返回新的新的响应头： return {responseHeaders: details.responseHeaders};
+ *   6、Chrome 新增的可信类型（Trusted types），暂时不知道怎么解决
+ *
+ */
+
 chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    tabinfo.set(details.tabId, hasCSP(details.responseHeaders));
+    (details)=>{
+        tabinfo.set(details.tabId, hasCSP(details.responseHeaders)); //暂时不知道什么地方用到
+        return {
+            responseHeaders:details.responseHeaders.filter(
+                header =>!removeCSP.includes(header.name.toLowerCase())
+            )
+        };
   },
   {
-    urls: ["<all_urls>"],
-    types: ["main_frame"],
+//    urls: ["<all_urls>"],
+    //需要移除CSP自己添加url
+    urls: [
+        "*://ajax.googleapis.com/*",
+        "*://fonts.googleapis.com/*",
+        "*://themes.googleusercontent.com/*",
+        "*://fonts.gstatic.com/*",
+        "*://*.google.com/*",
+        "*://secure.gravatar.com/*",
+        "*://www.gravatar.com/*",
+        "*://maxcdn.bootstrapcdn.com/*",
+        '*://api.github.com/*',
+        '*://www.gstatic.com/*',
+        '*://stackoverflow.com/*',
+        '*://translate.googleapis.com/*',
+        "*://developers.redhat.com/*",
+        "*://cloud-soft.xieyaokun.com/*"
+    ],
+    types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
   },
-  ["responseHeaders"]
+  ["blocking", 'responseHeaders']
 );
 
 chrome.webRequest.onBeforeRequest.addListener(
@@ -66,6 +120,7 @@ chrome.webRequest.onBeforeRequest.addListener(
       "*://secure.gravatar.com/*",
       "*://www.gravatar.com/*",
       "*://maxcdn.bootstrapcdn.com/bootstrap/*",
+
     ],
   },
   ["blocking"]
