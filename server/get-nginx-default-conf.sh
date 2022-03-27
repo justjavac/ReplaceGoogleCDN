05683d177c3369ae5073a0c5f6baf6339a386e00
jingjingxyk <zonghengbaihe521@qq.com>
2022-03-27 21:22:44 +0800

    补上80端口配置

--- server/get-nginx-default-conf.sh
+++ server/get-nginx-default-conf.sh
@@ -7,6 +7,7 @@ cd ${__DIR__}
 # 获得nginx 默认配置文件
 container_id=$(docker create nginx:alpine)  # returns container ID
 docker cp "$container_id":/etc/nginx/nginx.conf nginx.conf
+docker cp "$container_id":/etc/nginx/conf.d/ conf.d
 
 # shellcheck disable=SC2086
 docker rm $container_id
