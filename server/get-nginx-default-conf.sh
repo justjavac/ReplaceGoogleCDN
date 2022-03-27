4589f9e0ce51209fafcaf9338a1360856a4ad801
jingjingxyk <zonghengbaihe521@qq.com>
2022-03-27 20:43:23 +0800

    add docker run nginx server

---
+++ server/get-nginx-default-conf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+__DIR__=$(cd "$(dirname "$0")";pwd)
+cd ${__DIR__}
+
+# 获得nginx 默认配置文件
+container_id=$(docker create nginx:alpine)  # returns container ID
+docker cp "$container_id":/etc/nginx/nginx.conf nginx.conf
+
+# shellcheck disable=SC2086
+docker rm $container_id
---
+++ server/run-server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux
+
+__DIR__=$(cd "$(dirname "$0")";pwd)
+cd "${__DIR__}"
+
+# tls 目录存放 https 证书
+
+docker run --rm  \
+--name nginx-proxy-server \
+-p 8081:80 \
+-p 443:443 \
+-v "${__DIR__}"/nginx-ok.conf:/etc/nginx/nginx.conf \
+-v "${__DIR__}"/custom-proxy-header.item:/etc/nginx/conf.d/custom-proxy-header.item \
+-v "${__DIR__}"/hidden_proxy_headers.item:/etc/nginx/conf.d/hidden_proxy_headers.item \
+-v "${__DIR__}"/tls:/tls/ \
+nginx:alpine
