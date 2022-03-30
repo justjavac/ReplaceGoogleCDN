ae1b33731c1928e94010c4a62e76bb850714fab9
jingjingxyk <zonghengbaihe521@qq.com>
2022-03-30 11:27:19 +0800

    新增python调用chrome remote debug 接口

---
+++ extension/test/chromium-tools/python-chromium-remote-debug-api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+#  Chrome DevTools Protocol
+# https://chromedevtools.github.io/devtools-protocol/#endpoints
+
+#  http://127.0.0.1:9222/json/version
+#  http://127.0.0.1:9222/json/list
+#  http://127.0.0.1:9222/json/protocol/
+#  http://127.0.0.1:9222/json/new?{url}
+import request
+
