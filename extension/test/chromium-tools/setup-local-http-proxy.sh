402d1abdbb00a1b69c93781c27a390ad0b55aac7
jingjingxyk <zonghengbaihe521@qq.com>
2022-03-30 11:10:57 +0800

    change proxy port

--- extension/test/chromium-tools/setup-local-http-proxy.sh
+++ extension/test/chromium-tools/setup-local-http-proxy.sh
@@ -10,4 +10,4 @@ cd ${__DIR__}
 
 # 启动 本地 http 代理
 
-socat -d -d TCP4-LISTEN:8015,reuseaddr,fork ssl:tls.proxy.xiaoshuogeng.com:8015,verify=1
+socat -d -d TCP4-LISTEN:8015,reuseaddr,fork ssl:tls.proxy.xiaoshuogeng.com:8029,verify=1
