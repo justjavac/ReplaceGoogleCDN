c93e8dbbf56d2f11f55668d2af18d1df9bb16c05
jingjingxyk <zonghengbaihe521@qq.com>
2022-05-05 11:06:26 +0800

    test sw.js

--- extension/test/extension-v3-test/js/sw.js
+++ extension/test/extension-v3-test/js/sw.js
@@ -1,11 +1,17 @@
 
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
 // * chrome      - the global namespace for Chrome's extension APIs
 // * runtime     – the namespace of the specific API we want to use
 // * onInstalled - the event we want to subscribe to
 // * addListener - what we want to do with this event
 
 chrome.runtime.onInstalled.addListener(async () => {
-    console.log( "onInstalled do nothing ")
+    console.log( "chrome.runtime.onInstalled do nothing ")
     console.log(chrome.runtime.getURL("js/sw.js"))
     console.log(chrome.runtime.getURL("js/myscript.js"))
     console.log(chrome.runtime.getURL("js/tools/self-define-browser-editor.js"))
@@ -14,16 +20,24 @@ chrome.runtime.onInstalled.addListener(async () => {
 });
 
 chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
-    console.log( "oonUpdated do nothing ")
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) { }
+})
 
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+chrome.tabs.onCreated.addListener( (tab) => {
+    console.log(tab)
+})
+chrome.tabs.onActivated.addListener( (activeInfo) => {
+    console.log( "chrome.tabs.onActivated do nothing ")
+    console.log(activeInfo)
+
+
 
         //  注入JS 或者CSS 解决 PJAX 不可用问题等问题
-        console.log(tab.url)
+        console.log(chrome.runtime.getURL("js/tools/myscript-tools.js"))
         try{
             chrome.scripting.executeScript(
                                 {
-                                    target: {tabId: tabId},
+                                    target: {tabId: activeInfo.tabId},
                                     files: ["js/myscript.js"]
                                 },
                                 (injectionResults=null) => {
@@ -32,7 +46,7 @@ chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
                             )
             chrome.scripting.insertCSS(
                                 {
-                                    target: {tabId: tabId},
+                                    target: {tabId: activeInfo.tabId},
                                     files: ["css/my.css"]
                                 },
                                 (injectionResults=null) => {
@@ -46,6 +60,6 @@ chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
                 message.innerText = 'There was an error : \n' + chrome.runtime.lastError.message;
             }
         }
-    }
+
 
 });
