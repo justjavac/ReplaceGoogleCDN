37a786e2bb372a1eb2e758bb58009e2ca5d35819
jingjingxyk <zonghengbaihe521@qq.com>
2022-09-30 18:22:37 +0800

    完善扩展选项页

--- extension/options_ui/js/app.js
+++ extension/options_ui/js/app.js
@@ -25,7 +25,7 @@ let rules = {
     "/rules/rules_remove_content_security_policy_header.json",
 };
 
-let updateRule = (ruleinfo) => {
+let addRule = (ruleinfo) => {
   let id = 1;
   let domain = "example.com";
   chrome.declarativeNetRequest.updateDynamicRules({
@@ -46,9 +46,20 @@ let getRuleList = () => {
     console.log(count);
   });
 
+  chrome.declarativeNetRequest.getDynamicRules((rules) => {
+    console.log(rules);
+    let list_box = document.querySelector(".rule_dynamic_set_list");
+    let list = "";
+    rules.forEach((value, key, array) => {
+      console.log(value.id, value);
+      list += `<li data-rule="${value.id}">${value.id}</li>`;
+    });
+    list_box.innerHTML = list;
+  });
+
   chrome.declarativeNetRequest.getEnabledRulesets((rulesetIds) => {
     console.log(rulesetIds);
-    let list_box = document.querySelector(".rule_set_list");
+    let list_box = document.querySelector(".rule_static_set_list");
     let list = "";
     rulesetIds.map((value, index) => {
       list += `<li data-rule="${value}">${value}</li>`;
@@ -57,7 +68,7 @@ let getRuleList = () => {
   });
 
   document
-    .querySelector(".rule_set_list")
+    .querySelector(".rule_static_set_list")
     .addEventListener("click", (event) => {
       console.log(event.target);
       console.log(event.target.nodeType);
@@ -67,12 +78,38 @@ let getRuleList = () => {
       }
     });
 
+  /*
+      if (location.href.indexOf("problematic/url") !== -1) {
+        chrome.declarativeNetRequest.updateEnabledRulesets({"disableRulesetIds": ["rules"]});
+      } else {
+        chrome.declarativeNetRequest.updateEnabledRulesets({"enableRulesetIds": ["rules"]});
+      }
+
+     */
+
   return;
   chrome.declarativeNetRequest.getDynamicRules((rules) => {
     console.log(rules);
   });
   chrome.declarativeNetRequest.getMatchedRules({}, (RulesMatchedDetails) => {
     console.log(RulesMatchedDetails);
+  });
+};
+
+let deleteDynamicRules = () => {
+  chrome.declarativeNetRequest.getDynamicRules((rules) => {
+    console.log(rules);
+    let del_ids = [];
+    rules.forEach((value, key, array) => {
+      console.log(value, value.id);
+      del_ids.push(value.id);
+    });
+    if (del_ids) {
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [],
+        removeRuleIds: del_ids,
+      });
+    }
   });
 };
 
@@ -84,6 +121,134 @@ let getRuleList = () => {
     addClass,
     removeClass,
     createJSONFile,
+    fetchAll,
+    getContent,
   } = await import("/third_party/frontend-utils/utils.js");
   getRuleList();
+
+  document
+    .querySelector(".delete-sync-remote-rule")
+    .addEventListener("click", (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      deleteDynamicRules();
+    });
+
+  document
+    .querySelector(".goto-sync-remote-rule")
+    .addEventListener("click", async (event) => {
+      let rule_server_urls = document
+        .querySelector(".remote-rule-urls")
+        .value.trim();
+      let rules_urls = rule_server_urls.split("\n");
+      let new_rules_urls = [];
+      rules_urls.forEach((value, index, array) => {
+        value = value.trim();
+        value = value.replace(/^'|'$|,|，|。/g, "");
+        if (value.length > 1) {
+          console.log(value);
+          new_rules_urls.push(value);
+        }
+      });
+      console.log(new_rules_urls);
+      let default_rules_urls = [
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/auth.json?raw=true",
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/rules_advance_redirect_1.json?raw=true",
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/rules_advance_redirect_2.json?raw=true",
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/rules_block_request.json?raw=true",
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/rules_redirect_extra.json?raw=true",
+        "https://github.com/jingjingxyk/extension-v3-test/blob/main/rules/rules_remove_content_security_policy_header.json?raw=true",
+      ];
+      default_rules_urls = [
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/auth.json?raw=true",
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/rules_advance_redirect_1.json?raw=true",
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/rules_advance_redirect_2.json?raw=true",
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/rules_block_request.json?raw=true",
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/rules_redirect_extra.json?raw=true",
+        "https://www.jingjingxyk.com/chromium-extension/extension-v3-test/rules/rules_remove_content_security_policy_header.json?raw=true",
+      ];
+      default_rules_urls = [];
+      rules_urls = new_rules_urls ? new_rules_urls : default_rules_urls;
+
+      let result = await fetchAll(rules_urls, getContent);
+      console.log(result);
+      if (result.length > 1) {
+        deleteDynamicRules();
+
+        let dynamic_id_index = parseInt(new Date().getTime() / 1000);
+        let need_rules = [];
+        result.forEach((rules) => {
+          rules.forEach((rule, index, array) => {
+            console.log(rule);
+            rule.id = ++dynamic_id_index;
+            console.log(rule);
+            need_rules.push(rule);
+          });
+        });
+        console.log(need_rules);
+        chrome.declarativeNetRequest.updateDynamicRules(
+          {
+            addRules: need_rules,
+            removeRuleIds: [],
+          },
+          (info) => {
+            console.log(info);
+          }
+        );
+      }
+    });
+  document.querySelector(".add-rule").addEventListener("click", (event) => {
+    let rule_str = document.querySelector(".new-add-rule-pannel").value;
+    rule_str = rule_str.trim();
+    if (rule_str.length) {
+      rule_str = JSON.parse(rule_str);
+      if (rule_str) {
+        let need_rules = [];
+        let dynamic_id_index = parseInt(new Date().getTime() / 1000);
+        rule_str.forEach((value, key, array) => {
+          value.id = ++dynamic_id_index;
+          need_rules.push(value);
+        });
+        console.log(need_rules);
+        chrome.declarativeNetRequest.updateDynamicRules(
+          {
+            addRules: need_rules,
+            removeRuleIds: [],
+          },
+          (info) => {
+            console.log(info);
+          }
+        );
+      }
+    }
+  });
+  /*
+    let url = chrome.runtime.getURL("sandbox/index.html");
+        chrome.tabs.create({url}, (callback) => {
+        console.log(callback)
+    });
+    let iframe_src = document.querySelector("#external_page").getAttribute('src')
+    */
+  window.addEventListener(
+    "message",
+    (event) => {
+      console.log(event, event.source);
+      event.source.postMessage("hi there yourself!  the secret response ", "*");
+      //event.source.postMessage("hi there yourself!  the secret response ",event.origin);
+      //event.source.postMessage("hi there yourself!  the secret response ",location.origin+iframe_src);
+      console.log(event.data);
+      if (event.origin === "null") {
+        let data = JSON.parse(event.data);
+        console.log(data);
+
+        if (data.url) {
+          let url = data.url;
+          chrome.tabs.create({ url }, (callback) => {
+            console.log(callback);
+          });
+        }
+      }
+    },
+    false
+  );
 })();
