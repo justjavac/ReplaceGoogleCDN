4685d7584c8118665a2b102f6d2e9bedbb7244d3
好吧，你想说啥 <zonghengbaihe521@qq.com>
2022-09-30 22:20:17 +0800

    扩展选项页完善-第二版 (#102)
    * 重新启用对firefox 的支持和衍生新仓库
    
    * 新增 高阶定制玩法 辅助工具 新增 扩展程序选项 使用 prettier 格式化代码
    
    * change options_ui
    
    * test update-auth.sh
    
    * save update-domain.sh changed
    
    * save change
    
    * fix error
    
    * fix error
    
    * add public static library
    
    * fix error
    
    * 修改打包程序,打包时去除无用文件
    
    * fix test/READM.md test
    
    * change v2 version
    
    * change auto deploy.yaml setup
    
    * change release-archive.sh
    
    * 扩展选项:添加规则功能暂时屏蔽
    
    * 完善扩展选项页
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善自定义规则
    
    * 完善扩展选项页
    
    * 完善扩展选项页
    
    * 完善扩展选项页
    
    * 完善扩展选项页
    
    * 完善扩展选项页

---
+++ extension/sandbox/js/app.js
@@ -0,0 +1,29 @@
+let preview_url = [
+  "https://www.chromium.org/chromium-projects/",
+  "https://source.chromium.org/",
+  "https://summerofcode.withgoogle.com/programs/2022/organizations",
+  "https://cs.opensource.google/",
+  "https://webrtc.org/",
+  "https://chromium.googlesource.com/external/webrtc",
+];
+let list_box = document.querySelector(".box");
+let list = "";
+preview_url.map((value, index) => {
+  list += `<li data-href="${value}" ><pre>${value}</pre></li>`;
+});
+list_box.innerHTML = list;
+
+list_box.addEventListener("click", (event) => {
+  //console.log(event.target);
+  //console.log(event.target.nodeName);
+  if (event.target.nodeName === "PRE") {
+    let url = event.target.innerText;
+    //top.open(url, "_blank");
+    window.parent.postMessage(
+      JSON.stringify({
+        url: url,
+      }),
+      location.origin + "/options_ui/index.html"
+    );
+  }
+});
