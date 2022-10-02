943257300096955df8138f39216f593c6f46923c
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-02 18:58:18 +0800

    add chrome action open extension option page

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,1 +1,17 @@
-chrome.runtime.onInstalled.addListener((details) => {});
+chrome.runtime.onInstalled.addListener((details) => {
+
+});
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({url: "https://www.jingjingxyk.com"});
+    chrome.runtime.openOptionsPage((w)=>{
+        console.log(w)
+    })
+    /*
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['content.js']
+    });
+
+     */
+});
