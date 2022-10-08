9e5735694f68164bbb7015f11c71d2a7af8b3b9b
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-08 17:54:47 +0800

    修改打包程序

--- release-archive.sh
+++ release-archive.sh
@@ -11,13 +11,13 @@ test -d dist && rm -rf dist
 mkdir -p dist/
 cd ${__DIR__}/dist
 
-test -f extension-v2.zip && rm -f extension-v2.zip
-test -f extension-v3.zip && rm -f extension-v3.zip
+test -f ReplaceGoogleCDN-v2.zip && rm -f ReplaceGoogleCDN-v2.zip
+test -f ReplaceGoogleCDN-v3.zip && rm -f ReplaceGoogleCDN-v3.zip
 
 cd ${__DIR__}/extension
 
 # 打包 manifest v3 支持chromium 内核系列
-zip -r ../dist/extension-v3.zip . \
+zip -r ../dist/ReplaceGoogleCDN-v3.zip . \
   -x "tools/*" \
   -x "_metadata/*" \
   -x "rules/advance-no-use/*" \
@@ -33,24 +33,24 @@ zip -r ../dist/extension-v3.zip . \
   -x "third_party/webrtc/*"
 
 cd ${__DIR__}
-zip -u dist/extension-v3.zip ./README.md
-zip -u dist/extension-v3.zip ./Privacy.md
-zip -u dist/extension-v3.zip ./LICENSE
+zip -u dist/ReplaceGoogleCDN-v3.zip ./README.md
+zip -u dist/ReplaceGoogleCDN-v3.zip ./Privacy.md
+zip -u dist/ReplaceGoogleCDN-v3.zip ./LICENSE
 
 # 打包 manifest v2  支持chromium 内核系列 和 firefox
 cd ${__DIR__}/extension-v2/
 
-zip -r ../dist/extension-v2.zip . \
+zip -r ../dist/ReplaceGoogleCDN-v2.zip . \
   -x "./README.md"
 
 cd ${__DIR__}
-zip -u dist/extension-v2.zip ./README.md
-zip -u dist/extension-v2.zip ./Privacy.md
-zip -u dist/extension-v2.zip ./LICENSE
+zip -u dist/ReplaceGoogleCDN-v2.zip ./README.md
+zip -u dist/ReplaceGoogleCDN-v2.zip ./Privacy.md
+zip -u dist/ReplaceGoogleCDN-v2.zip ./LICENSE
 
 cd ${__DIR__}/dist
 # 查看打包结果
-unzip extension-v2.zip -d extension-v2
-unzip extension-v3.zip -d extension-v3
+unzip ReplaceGoogleCDN-v2.zip -d ReplaceGoogleCDN-v2
+unzip ReplaceGoogleCDN-v3.zip -d ReplaceGoogleCDN-v3
 
 cd ${__DIR__}
