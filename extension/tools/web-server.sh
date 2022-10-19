a3b8eb97101b03dbfd4d94bd715b1c0a79850b9c
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-19 22:59:57 +0800

    save changed

--- extension/tools/web-server.sh
+++ extension/tools/web-server.sh
@@ -11,19 +11,20 @@ echo ${__DIR__}
 __ROOT__=$(readlink -f ${__DIR__}/../../)
 echo ${__ROOT__}
 
+
 __WEB_DIR__=$(
-  cd ${__DIR__}/../
+  cd ${__DIR__}/web/
   pwd
 )
 echo ${__WEB_DIR__}
 
 
-cd ${__WEB_DIR__}/web/
+cd ${__WEB_DIR__}
 
 # 用于 解决google 翻译不能用的
 
 # 原理： https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Proxy_servers_and_tunneling/Proxy_Auto-Configuration_PAC_file
 
-cp proxy.js proxy.pac
+cp -f proxy.js proxy.pac
 
 python3 -m http.server 8000
