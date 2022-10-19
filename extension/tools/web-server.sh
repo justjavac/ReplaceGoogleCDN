94d73028803c0d91e68902135c4d95d6664cca80
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-19 22:52:09 +0800

    提供web server 用于测试同步规则

---
+++ extension/tools/web-server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -exu
+
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+echo ${__DIR__}
+
+
+__ROOT__=$(readlink -f ${__DIR__}/../../)
+echo ${__ROOT__}
+
+__WEB_DIR__=$(
+  cd ${__DIR__}/../
+  pwd
+)
+echo ${__WEB_DIR__}
+
+
+cd ${__WEB_DIR__}/web/
+
+# 用于 解决google 翻译不能用的
+
+# 原理： https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Proxy_servers_and_tunneling/Proxy_Auto-Configuration_PAC_file
+
+cp proxy.js proxy.pac
+
+python3 -m http.server 8000
