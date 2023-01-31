5ed5e8b3be652f6f4d43374fbb36c9ecabe5d288
jingjingxyk <zonghengbaihe521@qq.com>
2023-01-31 16:11:45 +0800

    移除无效的静态资源库和修改测试例子链接

--- extension/sandbox/js/app.js
+++ extension/sandbox/js/app.js
@@ -1,16 +1,37 @@
+/**
+ * 完善本扩展的初衷，获取知识
+ * 靡不有初，鲜克有终
+ * @type {string[]}
+ */
 let preview_url = [
+  "https://stackoverflow.com/tags/socat/hot?filter=all",
+  "https://patrickhlauke.github.io/recaptcha/",
+  "https://pub.dev/",
+  "https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js",
+  "https://developers.google.com/",
+  "https://releases.jquery.com/",
   "https://source.chromium.org/",
-  "https://summerofcode.withgoogle.com/programs/2022/organizations",
+  "https://cs.opensource.google/",
   "https://www.chromium.org/chromium-projects/",
+  "https://summerofcode.withgoogle.com/programs/2022/organizations",
   "https://webrtc.org/",
   "https://chromium.googlesource.com/external/webrtc",
-  "https://android.googlesource.com/",
-  "https://cs.opensource.google/",
 ];
+
+let is_iframe = self !== top;
 let list_box = document.querySelector(".box");
 let list = "";
 preview_url.map((value, index) => {
-  list += `<li data-href="${value}" ><pre>${value}</pre></li>`;
+  if (is_iframe) {
+    list += `<li data-href="${value}" ><pre>${value}</pre></li>`;
+  } else {
+    list += `<li data-href="${value}" >
+            <a
+              target="_blank" 
+              rel="noopener noreferrer"
+              href="${value}"
+            >${value}</a></li>`;
+  }
 });
 list_box.innerHTML = list;
 
@@ -19,8 +40,8 @@ list_box.addEventListener("click", (event) => {
   //console.log(event.target.nodeName);
   if (event.target.nodeName === "PRE") {
     let url = event.target.innerText;
-    console.log(self, top);
-    if (self !== top) {
+    //console.log(self, top);
+    if (is_iframe) {
       //'在iframe中'
       console.log("在iframe中");
       window.parent.postMessage(
