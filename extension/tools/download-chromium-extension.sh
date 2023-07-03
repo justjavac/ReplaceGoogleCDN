5d2e773bfdbcd113729ab72759e89662a86541d9
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-03 17:23:59 +0800

    升级svelte-jsoneditor v0.17.8 版本

--- extension/tools/download-chromium-extension.sh
+++ extension/tools/download-chromium-extension.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -exu
 
-__DIR__=$(cd "$(dirname "$0")";pwd)
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 cd ${__DIR__}
 
 if [ ! "$BASH_VERSION" ]; then
@@ -9,26 +12,34 @@ if [ ! "$BASH_VERSION" ]; then
   exit 1
 fi
 
-# 执行脚本命令
-
-# bash  extension/tools/download-chromium-extension.sh  --proxy 1
-
+# 本脚本存在的意义：从扩展应用商店下载扩展
 
 # 下载chromium 扩展，并解压
 # 例子 下载 谷歌翻译扩展
 # https://chrome.google.com/webstore/detail/google-translate/aapbdbdomjkkjkaonfhkkikfgjllcleb
-
 
 # 参考文档： https://github.com/tonystark93/crx-download/blob/master/background.js
 # 参考文档： https://www.cnblogs.com/jingjingxyk/p/16821342.html
 
 # `https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${version}&acceptformat=crx2,crx3&x=id%3D${result[1]}%26uc&nacl_arch=${nacl_arch}`;
 
+# bash  extension/tools/download-chromium-extension.sh  --proxy http://127.0.0.1:8015
 
-PROXY_URL=${2:+'http://127.0.0.1:8015'}
+mirror=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    export NO_PROXY="127.0.0.1,localhost,127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16,198.18.0.0/15,169.254.0.0/16"
+    export NO_PROXY="${NO_PROXY},localhost,.npmmirror.com,.aliyuncs.com,.taobao.org,.tsinghua.edu.cn,.ustc.edu.cn,.aliyun.com"
+    shift
+    ;;
+  *) ;;
 
-export http_proxy=$PROXY_URL
-export https_proxy=$PROXY_URL
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
 
 test -d temp && rm -rf temp
 mkdir -p temp
@@ -50,20 +61,16 @@ file_name='Multi-Elasticsearch-Head'
 extension_id=kpampjmfiopfpkkepbllemkibefkiice
 file_name='replace-google-cdn'
 
-download_url="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=109.0.5414.119&acceptformat=crx2,crx3&x=id%3D${extension_id}%26uc&nacl_arch=x86-64"
+download_url="https://clients2.google.com/service/update2/crx?response=redirect&prodversion=114.0.5726.0&acceptformat=crx2,crx3&x=id%3D${extension_id}%26uc&nacl_arch=x86-64"
 
 curl -Lo "${file_name}.crx" $download_url
-
-unset http_proxy
-unset https_proxy
-unset no_proxy
-
 
 # 使用不同的代理方式
 # proxychains curl -Lo google-translate.crx $download_url
 # curl -x "socks5h://127.0.0.1:2000" -Lo google-translate.crx $download_url
 # curl --proxy "socks5h://127.0.0.1:2000" -Lo google-translate.crx $download_url
 
+# crx 是经过定制的 zip 压缩格式文件
 set +e
 unzip -d ${file_name} "${file_name}.crx"
 set -e
@@ -71,7 +78,5 @@ set -e
 cd ${__DIR__}/
 
 exit 0
-
-
 
 test -d temp && rm -rf temp
--- extension/tools/update-library.sh
+++ extension/tools/update-library.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -exu
 
-__DIR__=$(cd "$(dirname "$0")";pwd)
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 cd ${__DIR__}
 
 if [ ! "$BASH_VERSION" ]; then
@@ -11,37 +14,49 @@ fi
 
 # test -d temp && rm -rf temp
 
-
 # example use proxy download source code
-# shell之变量默认值  https://blog.csdn.net/happytree001/article/details/120980066
 # 使用代理下载源码
-# bash  update-library.sh --proxy 1
+# bash  update-library.sh --proxy http://127.0.0.1:1080
 
-PROXY_URL=${2:+'http://127.0.0.1:8015'}
+mirror=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    export NO_PROXY="127.0.0.1,localhost,127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16,198.18.0.0/15,169.254.0.0/16"
+    export NO_PROXY="${NO_PROXY},localhost,.npmmirror.com,.aliyuncs.com,.taobao.org,.tsinghua.edu.cn,.ustc.edu.cn,.aliyun.com"
+    shift
+    ;;
+  *) ;;
 
-export http_proxy=$PROXY_URL
-export https_proxy=$PROXY_URL
-export no_proxy='0.0.0.0/8,10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,233.252.0.0/24,240.0.0.0/4,255.255.255.255/32,localhost,.npmmirror.com,.aliyuncs.com,.taobao.org,.tsinghua.edu.cn,.ustc.edu.cn,.aliyun.com'
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
 mkdir -p temp
 cd ${__DIR__}/temp
 
+VENDOR=${__DIR__}/../third_party/
 
-test -d frontend-utils/.git || git clone -b main https://github.com/jingjingxyk/frontend-utils.git  --depth=1 --progress
+test -d frontend-utils/.git || git clone -b main https://github.com/jingjingxyk/frontend-utils.git --depth=1 --progress
 
-mkdir -p ${__DIR__}/../third_party/jingjingxyk/frontend-utils
-cp -f frontend-utils/utils.js ${__DIR__}/../third_party/jingjingxyk/frontend-utils/utils.js
-
+mkdir -p ${VENDOR}/jingjingxyk/frontend-utils
+cp -f frontend-utils/utils.js ${VENDOR}/jingjingxyk/frontend-utils/utils.js
 
 cd ${__DIR__}/temp
-test -d svelte-jsoneditor/.git || git clone -b main https://github.com/josdejong/svelte-jsoneditor.git --depth=1 --progress
-mkdir -p ${__DIR__}/../third_party/josdejong/svelte-jsoneditor/main
+JSON_VERSION=v0.17.8
+test -d svelte-jsoneditor/.git || git clone -b ${JSON_VERSION} https://github.com/josdejong/svelte-jsoneditor.git --depth=1 --progress
+
 cd svelte-jsoneditor
 npm install
 npm run build
-cp -rf package-vanilla/* ${__DIR__}/../third_party/josdejong/svelte-jsoneditor/main
 
+rm -rf ${VENDOR}/josdejong/svelte-jsoneditor/
+mkdir -p ${VENDOR}/josdejong/svelte-jsoneditor/main
+
+cp -rf package-vanilla/* ${VENDOR}/josdejong/svelte-jsoneditor/main
 
 cd ${__DIR__}/
-
 
 test -d temp && rm -rf temp
