f64e63a0ddc069b6b987b5a680fa017834bb939b
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-04 21:06:52 +0800

    save changed

--- extension/js/background.js
+++ extension/js/background.js
@@ -15,15 +15,37 @@ browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
 */
 
 
-chrome.runtime.onInstalled.addListener((details) => {});
+chrome.runtime.onInstalled.addListener(async (details) => {
+    /*
+      cosnole.log(details)
+      switch (details) {
+        case "install":
+        {
+          let permissions = {
+            "origins": ['<all_urls>'],  //匹配模式  https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns
+            "permissions":[  //权限 https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/permissions/Permissions
+                "declarativeNetRequest" ,
+                "declarativeNetRequestFeedback" ,
+                "declarativeNetRequestWithHostAccess"
+            ]
+          }
+          let requesting = await browser.permissions.request(permissions)
+          console.log(requesting)
+        }
+          break;
+          // see below
+      }
+
+     */
+});
 
 chrome.action.onClicked.addListener((tab) => {
-  //通过action 打开选项页
-  //也可以打开指定网站，自动同步规则
+    //通过action 打开选项页
+    //也可以打开指定网站，自动同步规则
 
-  // 注释的原因：减少误打开选项页
-  // 默认侯选项切换已完成，现在可以打开了
-  chrome.runtime.openOptionsPage((w) => {
-    console.log(w);
-  });
+    // 注释的原因：减少误打开选项页
+    // 默认侯选项切换已完成，现在可以打开了
+    chrome.runtime.openOptionsPage((w) => {
+        console.log(w);
+    });
 });
