27803ea615ee72d649cf5a73fb3bb48514fe2a97
进栈检票 <stackinspector@126.com>
2023-10-11 08:36:59 +0800

    fix typo (#188)

--- extension/js/background.js
+++ extension/js/background.js
@@ -5,7 +5,7 @@ chrome.action.onClicked.addListener((tab) => {
   //也可以打开指定网站，自动同步规则
 
   // 注释的原因：减少误打开选项页
-  // 默认侯选项切换已完成，现在可以打开了
+  // 默认候选项切换已完成，现在可以打开了
   chrome.runtime.openOptionsPage((w) => {
     console.log(w);
   });
