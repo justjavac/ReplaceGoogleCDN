dccff887cada76d1c4d3531090882b0eac6b52cb
迷渡 <justjavac@gmail.com>
2026-07-01 16:51:06 +0800

    Guard options page opening

--- extension/js/background.js
+++ extension/js/background.js
@@ -6,7 +6,15 @@ chrome.action.onClicked.addListener((tab) => {
 
   // 注释的原因：减少误打开选项页
   // 默认候选项切换已完成，现在可以打开了
+  if (!chrome.runtime.getManifest().options_ui) {
+    return;
+  }
   chrome.runtime.openOptionsPage((w) => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError.message);
+      return;
+    }
+
     console.log(w);
   });
 });
